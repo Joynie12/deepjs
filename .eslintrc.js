93aae501aa19708b1adaed0ab3addfbbd3ee3d07
Joynie12 <bigw199412@gmail.com>
2023-03-20 10:57:40 +0300

    Ready to go

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement']
+  }
+}
